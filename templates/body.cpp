9fb4659b9b5b27d6f7c12a81c1d4d875d4110638
luismacostero <lcostero@ucm.es>
2017-10-17 15:31:48 +0200

    template proposal

---
+++ templates/body.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <iterator>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <map>
+#include <complex>
+#include <utility>
+#include <cmath>
+#include <cstring>
+#include <sstream>
+#include <climits>
+#include <algorithm>
+#include <iomanip>
+#include <bitset>
+#include <functional>
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<vi > vvi;
+
+typedef long long int lli;
+typedef unsigned long long int ull;
+typedef unsigned long ul;
+
+
+
+
+int main(){
+  
+  return 0;
+}
