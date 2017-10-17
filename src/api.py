8caa39814da3ce314c174f43bde84c0f2c9d0cc5
luismacostero <lcostero@ucm.es>
2017-10-17 18:53:15 +0200

    new api methods

--- src/api.py
+++ src/api.py
@@ -17,7 +17,7 @@ def get_user_id(username):
     url = uhunt_url + "uname2uid/" + username
 
     r = requests.get(url)
-    checkRequest(r)
+    check_request(r)
     if r.text == "0":
         raise Exception("User (" + username + ") not Found")
     return int(r.text)
@@ -26,13 +26,14 @@ def get_user_id(username):
 def get_problem_info(problemNumber):
     """Returns the information of a specific problem.
     """
-    if not isinstance(problemNumber, int):
-        raise ValueError('problemNumber is not an integer')
+
+    #TODO: check if the problem exists
+    check_integer(problemNumber)
 
     url = uhunt_url + "p/num/" + str(problemNumber)
 
     r = requests.get(url)
-    checkRequest(r)
+    check_request(r)
 
     rinfo = r.json()
     info = {}
@@ -42,12 +43,61 @@ def get_problem_info(problemNumber):
                        str(int(problemNumber / 100)) +
                        '/' + str(problemNumber) + '.pdf')
     info["title"] = rinfo["title"]
+    info["best_time"] = get_best_time(int(rinfo["pid"]))
     return info
 
 
-def checkRequest(request):
+def get_problemid_from_number(problemNumber):
+    """Returns the problemID assciated with the problem number"""
+    check_integer(problemNumber)
+
+    url = uhunt_url + "p/num/" + str(problemNumber)
+
+    r = requests.get(url)
+    check_request(r)
+
+    rjson = r.json()
+
+    return int(rjson["pid"])
+
+
+
+def get_best_time(ident, problemId=True):
+    """Given a problemId or ProblemNumber, returns the time of the best
+    solution.
+    problemId=True:    from the problemId
+             =False:   from the problemNumber
+
+    """
+    check_integer(ident)
+
+    if problemId:
+        number = ident
+    else:
+        number = get_problemid_from_number(ident)
+
+    url = uhunt_url + "p/rank/" + str(number) + "/1/1"
+
+    r = requests.get(url)
+    check_request(r)
+
+    rjson = r.json()
+    return int(rjson[0]["run"])
+
+
+    
+    
+def check_request(request):
     if request.status_code != 200:
         raise Exception("Return code error")
+
+
+
+def check_integer(number, msg=None):
+    """raise an exception if number is not an integer
+    """
+    if not isinstance(number, int):
+        raise ValueError(msg)
 
 
 
