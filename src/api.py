47b9ef5563acf6ea3c0066ac8f989f55770b7d5e
Jesus Domenech <jesusjda@gmail.com>
2017-10-17 18:25:28 +0200

    api renaiming and PEP8

--- src/api.py
+++ src/api.py
@@ -1,36 +1,34 @@
 import requests
 import json
 
-#Base urls for the services
-uhunt_url="http://uhunt.onlinejudge.org/api/"
-uva_url="http://uva.onlinejudge.org/"
+# Base urls for the services
+uhunt_url = "http://uhunt.onlinejudge.org/api/"
+uva_url = "http://uva.onlinejudge.org/"
 
-#specific urls for specific services
+# specific urls for specific services
 uva_pdfProblem_url = "external/"
 
 
-
-def getUserID(username):
-    """
-    Returns the user ID of the given username.
+def get_user_id(username):
+    """Returns the user ID of the given username.
     If the user does not exist, returns 0
     """
-    
+
     url = uhunt_url + "uname2uid/" + username
 
-    r = requests.get(url);
+    r = requests.get(url)
     checkRequest(r)
-
+    if r.text == "0":
+        raise Exception("User (" + username + ") not Found")
     return int(r.text)
 
 
-def getProblemInfo(problemNumber):
-    """
-    Returns the information of a specific problem.    
+def get_problem_info(problemNumber):
+    """Returns the information of a specific problem.
     """
     if not isinstance(problemNumber, int):
         raise ValueError('problemNumber is not an integer')
-    
+
     url = uhunt_url + "p/num/" + str(problemNumber)
 
     r = requests.get(url)
@@ -39,18 +37,13 @@ def getProblemInfo(problemNumber):
     rinfo = r.json()
     info = {}
 
+    info["pdf_url"] = (uva_url + uva_pdfProblem_url +
+                       str(int(problemNumber / 100)) +
+                       '/' + str(problemNumber) + '.pdf')
     info["number"] = rinfo["num"]
-    info["pdf_url"] = uva_url + uva_pdfProblem_url + str(int(problemNumber / 100) ) + '/' + str(problemNumber) + '.pdf'
-    
-    
     return info
 
 
 def checkRequest(request):
     if request.status_code != 200:
         raise Exception("Return code error")
-
-
-
-
-print (getProblemInfo(100))
