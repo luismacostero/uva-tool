bc3835003e65f18392d6222769b74988b65d361a
Jesus Domenech <jesusjda@gmail.com>
2017-11-21 18:23:08 +0100

    filemanager

--- src/config.py
+++ src/config.py
@@ -26,14 +26,14 @@ def read_config(path):
     return data
 
 
-def write_config(config):
+def write_config(config, path):
     # Writing JSON data
-    with open(config_path, 'w') as f:
+    with open(path, 'w') as f:
         json.dump(config, f)
 
 
 def init():
-    # PATH, USER_NAME, USER_ID, PASS?, URL
+    change = False
     try:
         config = read_config(config_path)
     except:
@@ -44,30 +44,44 @@ def init():
         private = {}
 
     if not("path" in config):
+        change = True
         config["path"] = ask("Where are you going to save your repo?")
 
     if not(filemanger.check_path(config["path"])):
         if not("repo" in config):
+            change = True
             config["repo"] = ask("Which is your git-repository url?")
         filemanager.create_repo_folder(config["repo"], config["path"])
 
+    if not("author" in config):
+        change = True
+        config["author"] = ask("Which is your author name?")
+
     if not("uva_user" in config):
+        change = True
         config["uva_user"] = ask("Which is your UVA user?")
     if not("uva_pass" in private):
+        change = True
         private["uva_pass"] = ask_private("Which is your UVA pass?")
 
     if not("acr_user" in config):
+        change = True
         config["acr_user"] = ask("Which is your ACR user?")
     if not("acr_pass" in config):
+        change = True
         private["acr_pass"] = ask_private("Which is your ACR pass?")
 
     # Api items
     # user_id
     if not("uva_id" in config) or config["uva_id"] == "":
+        change = True
         config["uva_id"] = api.uva.get_user_id(config["uva_user"])
     if not("acr_id" in config) or config["acr_id"] == "":
+        change = True
         config["acr_id"] = api.acr.get_user_id(config["acr_user"])
 
-    write_config(config)
+    if change:
+        write_config(config, config_path)
+        write_config(private, private_path)
 
     return config
--- src/filemanager.py
+++ src/filemanager.py
@@ -1,11 +1,48 @@
 # -*- coding: utf-8 -*-
 import os
+import git
+from subprocess import call
 basedir = os.getcwd()
 
 
-def check_path(path):
-    pass
+def _relative(path, other=""):
+    return os.path.join(basedir, os.path.join(path, other))
+
+
+def check_folder(path, foldername=""):
+    return os.path.isdir(_relative(path, foldername))
+
+
+def check_file(path, filename=""):
+    return os.path.exists(_relative(path, filename))
 
 
 def create_repo_folder(repo, path):
-    pass
+    """Clone repo into path
+    """
+    folder = _relative(path)
+    if not(check_folder(folder, ".git")):
+        git.Repo.clone_from(repo, folder)
+    else:
+        raise Exception("The folder is already used.")
+
+
+def open_problem(config, problemid, judge="uva"):
+    """Check if exists, if not create it, then open
+    """
+    directory = _relative(config["path"], judge+"/"+problemid)
+    if not(check_folder(directory)):
+        create_problem_folder(config["path"], problemid, judge=judge)
+        problemfile.create_problem(author=config["author"],
+                                   problemid=problemid,
+                                   judge=judge,
+                                   path=directory)
+    editos_open(config, problemid, judge=judge)
+
+
+def editor_open(config, problemid, judge="uva"):
+    path = _relative(config["path"], judge+"/"+problemid)
+    prefix = os.path.join(path, problemid+".")
+    extensions = ["cpp", "in", "out"]
+    args = ["emacs"] + [prefix+e for e in extensions] + ["&"]
+    call(args)
