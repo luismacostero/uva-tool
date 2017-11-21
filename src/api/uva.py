2c981ce37a88c2fd826fba51857bf590600b7c92
Jesus Domenech <jesusjda@gmail.com>
2017-11-21 16:39:16 +0100

    new init

---
+++ src/api/uva.py
@@ -0,0 +1,103 @@
+import requests
+import json
+
+# Base urls for the services
+uhunt_url = "http://uhunt.onlinejudge.org/api/"
+uva_url = "http://uva.onlinejudge.org/"
+
+# specific urls for specific services
+uva_pdfProblem_url = "external/"
+
+
+def get_user_id(username):
+    """Returns the user ID of the given username.
+    If the user does not exist, returns 0
+    """
+
+    url = uhunt_url + "uname2uid/" + username
+
+    r = requests.get(url)
+    check_request(r)
+    if r.text == "0":
+        raise Exception("User (" + username + ") not Found")
+    return int(r.text)
+
+
+def get_problem_info(problemNumber):
+    """Returns the information of a specific problem.
+    """
+
+    #TODO: check if the problem exists
+    check_integer(problemNumber)
+
+    url = uhunt_url + "p/num/" + str(problemNumber)
+
+    r = requests.get(url)
+    check_request(r)
+
+    rinfo = r.json()
+    info = {}
+
+    info["number"] = rinfo["num"]
+    info["pdf_url"] = (uva_url + uva_pdfProblem_url +
+                       str(int(problemNumber / 100)) +
+                       '/' + str(problemNumber) + '.pdf')
+    info["title"] = rinfo["title"]
+    info["best_time"] = get_best_time(int(rinfo["pid"]))
+    return info
+
+
+def get_problemid_from_number(problemNumber):
+    """Returns the problemID assciated with the problem number"""
+    check_integer(problemNumber)
+
+    url = uhunt_url + "p/num/" + str(problemNumber)
+
+    r = requests.get(url)
+    check_request(r)
+
+    rjson = r.json()
+
+    return int(rjson["pid"])
+
+
+
+def get_best_time(ident, problemId=True):
+    """Given a problemId or ProblemNumber, returns the time of the best
+    solution.
+    problemId=True:    from the problemId
+             =False:   from the problemNumber
+
+    """
+    check_integer(ident)
+
+    if problemId:
+        number = ident
+    else:
+        number = get_problemid_from_number(ident)
+
+    url = uhunt_url + "p/rank/" + str(number) + "/1/1"
+
+    r = requests.get(url)
+    check_request(r)
+
+    rjson = r.json()
+    return int(rjson[0]["run"])
+
+
+    
+    
+def check_request(request):
+    if request.status_code != 200:
+        raise Exception("Return code error")
+
+
+
+def check_integer(number, msg=None):
+    """raise an exception if number is not an integer
+    """
+    if not isinstance(number, int):
+        raise ValueError(msg)
+
+
+
--- src/config.py
+++ src/config.py
@@ -2,16 +2,21 @@
 import os
 import json
 import api
-
+import getpass
+import filemanager
 
 basedir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "..")
 config_path = os.path.join(basedir, "config.json")
+private_path = os.path.join(basedir, "private.json")
 
 
-def ask(key):
-    answer = raw_input("Give me the " + key + ": ")
-    return answer
+def ask(basestring msg):
+    return raw_input(msg + " ")
+
+
+def ask_private(basestring msg):
+    return getpass.getpass(msg + " ")
 
 
 def read_config(path):
@@ -33,15 +38,36 @@ def init():
         config = read_config(config_path)
     except:
         config = {}
+    try:
+        private = read_config(private_path)
+    except:
+        private = {}
 
-    # Set generic Items
-    keys = ["path", "user_name", "pass"]
-    for k in keys:
-        if not(k in config) or config[k] == "":
-            config[k] = ask(k)
+    if not("path" in config):
+        config["path"] = ask("Where are you going to save your repo?")
+
+    if not(filemanger.check_path(config["path"])):
+        if not("repo" in config):
+            config["repo"] = ask("Which is your git-repository url?")
+        filemanager.create_repo_folder(config["repo"], config["path"])
+
+    if not("uva_user" in config):
+        config["uva_user"] = ask("Which is your UVA user?")
+    if not("uva_pass" in private):
+        private["uva_pass"] = ask_private("Which is your UVA pass?")
+
+    if not("acr_user" in config):
+        config["acr_user"] = ask("Which is your ACR user?")
+    if not("acr_pass" in config):
+        private["acr_pass"] = ask_private("Which is your ACR pass?")
 
     # Api items
     # user_id
-    if not("user_id" in config) or config["user_id"] == "":
-        config["user_id"] = api.get_user_id(config["user_name"])
+    if not("uva_id" in config) or config["uva_id"] == "":
+        config["uva_id"] = api.uva.get_user_id(config["uva_user"])
+    if not("acr_id" in config) or config["acr_id"] == "":
+        config["acr_id"] = api.acr.get_user_id(config["acr_user"])
+
+    write_config(config)
+
     return config
